dc46860e4a173d17f119eac72f709ad190214638
Nirma Abro <NirmaAbro143@gmail.com>
2026-02-27 15:20:01 +0500

    pagination done on get all product

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -25,12 +25,32 @@ export const createProduct = async (req, res) => {
 
 export const getAllProducts = async (req, res) => {
   try {
-    const newProduct = await product.find();
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    if (page < 1) page = 1;
+    if (limit > 50) limit = 50;
+
+    const skip = (page - 1) * limit;
+    const totalProduct = await product.countDocuments();
+
+    const newProduct = await product
+      .find()
+      .sort({ createdAt: -1 })
+      .skip()
+      .limit();
+
+    const totalPages = Math.ceil(totalProduct / limit);
+
     res.status(200).json({
       success: true,
-      count: newProduct.length,
-      newProduct,
+      message: "All products",
+      totalPages,
+      totalProduct,
+      count : newProduct.length,
+      data: newProduct,
     });
+    
   } catch (error) {
     res.status(500).json({
       success: false,
